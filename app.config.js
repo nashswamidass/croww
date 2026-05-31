60ae90ca21e7706a32e52057cbdb090608db78c4
Nash Newton <nashnewton@192.168.0.135>
2026-05-31 12:46:46 +0530

    chore: increment ios buildNumber to 27

--- app.config.js
+++ app.config.js
@@ -12,7 +12,7 @@ export default {
     "ios": {
       "supportsTablet": true,
       "bundleIdentifier": "com.croww.app",
-      "buildNumber": "26",
+      "buildNumber": "27",
       "googleServicesFile": process.env.APP_ENV === 'staging' ? "./GoogleService-Info.staging.plist" : "./GoogleService-Info.plist",
       "config": {
         "googleMapsApiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
